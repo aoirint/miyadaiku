570aeec2a950b743c309352ed03fd5f0c7281c69
aoirint <aoirint@gmail.com>
2020-11-03 01:57:41 +0900

    strip directory index (related to #5)

--- miyadaiku/__init__.py
+++ miyadaiku/__init__.py
@@ -9,7 +9,7 @@ from typing import Any, Dict, List, NamedTuple, Optional, Sequence, Set, Tuple, 
 import importlib_resources
 import tzlocal
 
-__version__ = "1.22.0"
+__version__ = "1.22.0+2"
 
 YAML_ENCODING = "utf-8"
 
